511b456a6495c1890a96d5a4983e04dd0e7d4517
Abhishek Das <das.abhshk@gmail.com>
2019-03-09 17:24:16 +0000

    Minor fixes to helper script

--- utils/process.py
+++ utils/process.py
@@ -27,6 +27,7 @@ except ImportError:
 from yaml.representer import SafeRepresenter
 _mapping_tag = yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG
 
+
 def dict_representer(dumper, data):
     return dumper.represent_dict(data.iteritems())
 
@@ -34,27 +35,31 @@ def dict_representer(dumper, data):
 def dict_constructor(loader, node):
     return OrderedDict(loader.construct_pairs(node))
 
+
 Dumper.add_representer(OrderedDict, dict_representer)
 Loader.add_constructor(_mapping_tag, dict_constructor)
 
-Dumper.add_representer(str,
-                       SafeRepresenter.represent_str)
+Dumper.add_representer(str, SafeRepresenter.represent_str)
 
 
 def ordered_dump(data, stream=None, Dumper=yaml.Dumper, **kwds):
     class OrderedDumper(Dumper):
         pass
+
     def _dict_representer(dumper, data):
         return dumper.represent_mapping(
-            yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-            data.items())
+            yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, data.items())
+
     OrderedDumper.add_representer(OrderedDict, _dict_representer)
     return yaml.dump(data, stream, OrderedDumper, **kwds)
 
-dateformat = '%Y-%m-%d %H:%M:%S'
-tba_words = ["tba","tbd"]
 
-right_now = datetime.datetime.utcnow().replace(microsecond=0).strftime(dateformat)
+dateformat = '%Y-%m-%d %H:%M:%S'
+tba_words = ["tba", "tbd"]
+
+right_now = datetime.datetime.utcnow().replace(
+    microsecond=0).strftime(dateformat)
+
 
 # Helper function for yes no questions
 def query_yes_no(question, default="no"):
@@ -67,8 +72,7 @@ def query_yes_no(question, default="no"):
 
     The "answer" return value is True for "yes" or False for "no".
     """
-    valid = {"yes": True, "y": True, "ye": True,
-             "no": False, "n": False}
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
     if default is None:
         prompt = " [y/n] "
     elif default == "yes":
@@ -97,25 +101,29 @@ with open("../_data/conferences.yml", 'r') as stream:
         data = yaml.load(stream, Loader=Loader)
         print("Initial Sorting:")
         for q in data:
-            print(q["deadline"]," - ",q["title"])
+            print(q["deadline"], " - ", q["title"])
         print("\n\n")
         conf = [x for x in data if x['deadline'].lower() not in tba_words]
-        tba  = [x for x in data if x['deadline'].lower() in tba_words]
+        tba = [x for x in data if x['deadline'].lower() in tba_words]
 
         # just sort:
-        conf.sort(key=lambda x: pytz.utc.normalize(datetime.datetime.strptime(x['deadline'], dateformat).replace(tzinfo=pytz.timezone(x['timezone']))))
+        conf.sort(key=lambda x: pytz.utc.normalize(datetime.datetime.strptime(x['deadline'], dateformat).replace(tzinfo=pytz.timezone(x['timezone'].replace('UTC+', 'Etc/GMT-').replace('UTC-', 'Etc/GMT+')))))
         print("Date Sorting:")
-        for q in conf+tba:
-            print(q["deadline"]," - ",q["title"])
+        for q in conf + tba:
+            print(q["deadline"], " - ", q["title"])
         print("\n\n")
-        conf.sort(key=lambda x: pytz.utc.normalize(datetime.datetime.strptime(x['deadline'], dateformat).replace(tzinfo=pytz.timezone(x['timezone']))).strftime(dateformat) < right_now)
+        conf.sort(key=lambda x: pytz.utc.normalize(datetime.datetime.strptime(x['deadline'], dateformat).replace(tzinfo=pytz.timezone(x['timezone'].replace('UTC+', 'Etc/GMT-').replace('UTC-', 'Etc/GMT+')))).strftime(dateformat) < right_now)
         print("Date and Passed Deadline Sorting with tba:")
-        for q in conf+tba:
-            print(q["deadline"]," - ",q["title"])
+        for q in conf + tba:
+            print(q["deadline"], " - ", q["title"])
         print("\n\n")
 
         with open('sorted_data.yml', 'w') as outfile:
-            for line in ordered_dump(conf+tba, Dumper=yaml.SafeDumper, default_flow_style=False, explicit_start=True).splitlines():
+            for line in ordered_dump(
+                    conf + tba,
+                    Dumper=yaml.SafeDumper,
+                    default_flow_style=False,
+                    explicit_start=True).splitlines():
                 outfile.write('\n')
                 outfile.write(line.replace('- title:', '\n- title:'))
     except yaml.YAMLError as exc:
